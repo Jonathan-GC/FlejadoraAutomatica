9513ba2a650d2898e27e1b7abb5a2013223219a9
Jonathan-GC <jonathangc.awt@gmail.com>
2022-01-19 15:18:26 -0500

    Probando teclado

---
+++ botones/Prueba_teclado/Prueba_teclado.ino
@@ -0,0 +1,16 @@
+
+#define ANALOG_PIN A1
+
+
+enum Teclado {ENTER, LEFT, UP, DOWN, RIGHT, UNKNOWN};
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
