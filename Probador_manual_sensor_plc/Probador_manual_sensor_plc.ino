65487edbe4bebb80d1d5fdf0fb290021b795d908
joshuaolarte <joshuaolarte02@hotmail.com>
2022-01-20 09:03:44 -0500

    codigo adelantado de nuevas entradas y salidas en PC viejo

--- Probador_manual_sensor_plc/Probador_manual_sensor_plc.ino
+++ Probador_manual_sensor_plc/Probador_manual_sensor_plc.ino
@@ -1,5 +1,7 @@
 #include <avr/wdt.h> // Incluir la librería que contiene el watchdog (wdt.h)
-//#include <LiquidCrystal.h>
+#include <LiquidCrystal.h>
+#include <EEPROM.h>
+
 #define ConstanteDeConversion 10 //12.8 // con velocida de 26 en Driver Variador
 #define ConstanteDeConversionPulsos 0,0612612
 
@@ -11,6 +13,10 @@ volatile boolean stateChannelB;
 volatile boolean prevStateChannelA = 0;
 
 #define maxSteps 5000
+
+
+
+
 //const int maxSteps = 5000;
 volatile int prevValue;
 volatile int value;
@@ -35,12 +41,25 @@ int Fleje1 [6] = {1,10,10,10,10,22};
 #define d5 5 
 #define d6 6 
 #define d7 7
-//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-
+unsigned int cont_inicios = 0;
 void setup() {
    Serial.begin(9600);
+   lcd.begin(16, 2);
+   
+   cont_inicios = EEPROM.read(0);
+   cont_inicios++;
+   EEPROM.put(0, cont_inicios);
 
+  Serial.print("Re-Inicios\t");
+  Serial.print(cont_inicios, DEC);
+  Serial.println();
+
+  lcd.setCursor(0,0);
+  lcd.print("Re-Inicios ");
+  lcd.print(cont_inicios, DEC);
+   
    wdt_disable();// Desactivar el watchdog mientras se configura, para que no se resetee
    //wdt_enable(WDTO_4S);
    pinMode(channelPinA, INPUT_PULLUP);
@@ -50,7 +69,7 @@ void setup() {
    pinMode(A4, OUTPUT); // Pin I7 PLC para giro 45°
    pinMode(11, OUTPUT); // Pin I8 PLC para giro 90°
    pinMode(12, OUTPUT); // Pin I11 PLC para giro retraer
-   
+
    digitalWrite(A5, HIGH); //para apagarlos de inicio ya que es negado
    digitalWrite(A4, HIGH); //para apagarlos de inicio ya que es negado
    digitalWrite(11, HIGH); //para apagarlos de inicio ya que es negado
@@ -67,66 +86,70 @@ void setup() {
 //   delay(3000);
 }
 
+char dato = 0;
 void loop(){
 //  lcd.clear();
   //wdt_reset();
-  if(inOrden()){
-    for(int i = 0; i < 5; i++){
-        Serial.print("Giro: ");Serial.println(i+1);
-        //Toma medidas
-        int Medida = 0;
-        Medida = Fleje1[i];
-        Serial.println(deCmAPulsos(Medida));
-        Medida = deCmAPulsos(Medida);
-        
-        digitalWrite(A5, LOW);
-        //Alimente mientras este lleno
-        while(value < Medida){
-          Encoder();
-        }
-        digitalWrite(A5, HIGH);
-        value = 0;
+  if(!digitalRead(A1)){
+    Serial.println("Pulsado");
+  }
+  
+  if(Serial.available()>0){
+    dato = Serial.read();
+    Serial.print("dato: ");Serial.println(dato);
+  }
 
-        //Eleccion de ángulo
-        if(i == 0 || i == 4 ){
-          digitalWrite(A4, LOW);
-          delay(500);
-          digitalWrite(A4, HIGH);
-        }
-        
-        else{
-          digitalWrite(11, LOW);
-          delay(500);
-          digitalWrite(11, HIGH);
-        }
+  switch(dato){
+    case 'a':
+      digitalWrite(A5, 0);
+      delay(400);
+      digitalWrite(A5, 1);
+    break;
 
-        while(!inOrden());
-        
-        //Esperar al que el plc de la orden de 
-        //volver a arrancar
+    case 's':
+      digitalWrite(A5, 0);
+      delay(300);
+      digitalWrite(A5, 1);
+    break;
 
-        
-     }
-        //Reset en el contador encoder0
-        value = 0;
-        Serial.println("SAlio del for a corte");
-        //Toma medidas
-        int Medida = 0;
-        
-        Medida = Fleje1[5];
-        Serial.println(deCmAPulsos(Medida));
-        Medida = deCmAPulsos(Medida);
-        
-        digitalWrite(12, LOW);
-        //Alimente mientras este lleno
-        while(value < Medida){
-          Encoder();
-        }
-        digitalWrite(12, HIGH);
-        value = 0;
+    case '4':
+      digitalWrite(A4, 0);
+      delay(300);
+      digitalWrite(A4, 1);
+      
+    break;
 
-     //while(1);
-     
+    case '9':
+      digitalWrite(11, 0);
+      delay(300);
+      digitalWrite(11, 1);
+    break;
+
+    case 'r':
+      digitalWrite(12, 0);
+      delay(500);
+      digitalWrite(12, 1);
+    break;
+
+    case 'e':
+      Encoder();
+      break;
+
+    case 'b':
+      for (int i = 0 ; i < EEPROM.length() ; i++) {
+        EEPROM.write(i, 0);
+      }
+
+       cont_inicios = EEPROM.read(0);
+       
+      Serial.print("Re-Inicios\t");
+      Serial.print(cont_inicios, DEC);
+      Serial.println();
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Re-Inicios ");
+      lcd.print(cont_inicios, DEC);
+      break;
   }
   
   
@@ -135,7 +158,7 @@ void loop(){
 boolean inOrden(){
    if(digitalRead(A1) == 1){
       presionado = 1; 
-      delay(50);
+      delay(150);
    }
    if (digitalRead(A1) == 0 && presionado == 1){
       presionado = 0;
@@ -173,7 +196,7 @@ void Encoder (){
       if (prevValue != value)
       {
          prevValue = value;
-         //Serial.println(value);
+         Serial.println(value);
 
       }
 
